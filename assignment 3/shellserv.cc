cf06192d2932e5239948c1fcb424013f7029ccd4
Wing-Shu <57225457+Wing-Shu@users.noreply.github.com>
2021-10-28 23:17:16 -0400

    Update shellserv.cc

--- assignment 3/shellserv.cc
+++ assignment 3/shellserv.cc
@@ -15,6 +15,12 @@
 #include "tcp-utils.h"
 
 /*
+ * Global configuration variables.
+ */
+const char* path[] = {"/bin","/usr/bin",0}; // path, null terminated (static)
+char output[4096];
+
+/*
  * Typical reaper.
  */
 void zombie_reaper (int signal) {
@@ -35,56 +41,124 @@ void block_zombie_reaper (int signal) {
     /* NOP */
 }
 
-// use to write data on file history
-struct monitor_t {
-    pthread_mutex_t mutex;
-    char* line_ptr;
+int run_it (const char* command, char* const argv[], char* const envp[], const char** path) {
+
+    // we really want to wait for children so we inhibit the normal
+    // handling of SIGCHLD
+    signal(SIGCHLD, block_zombie_reaper);
+
+    int childp = fork();
+    int status = 0;
+
+    if (childp == 0) { // child does execve
+
+    //redirect output
+    // int file_no  = open("output.txt", O_WRONLY | O_CREAT, S_IRUSR|S_IWUSR);
+    // if (file_no == -1){
+    //     return -2;
+    // }
+    // int output_no = dup2(file, STDOUT_FILENO);
+    // close(file_no);
+
+#ifdef DEBUG
+        fprintf(stderr, "%s: attempting to run (%s)", __FILE__, command);
+        for (int i = 1; argv[i] != 0; i++)
+            fprintf(stderr, " (%s)", argv[i]);
+        fprintf(stderr, "\n");
+#endif
+        execve(command, argv, envp);     // attempt to execute with no path prefix...
+        for (size_t i = 0; path[i] != 0; i++) { // ... then try the path prefixes
+            char* cp = new char [strlen(path[i]) + strlen(command) + 2];
+            sprintf(cp, "%s/%s", path[i], command);
+#ifdef DEBUG
+            fprintf(stderr, "%s: attempting to run (%s)", __FILE__, cp);
+            for (int i = 1; argv[i] != 0; i++)
+                fprintf(stderr, " (%s)", argv[i]);
+            fprintf(stderr, "\n");
+#endif
+            execve(cp, argv, envp);
+            delete [] cp;
+        }
+
+        // If we get here then all execve calls failed and errno is set
+        char* message = new char [strlen(command)+10];
+        sprintf(message, "exec %s", command);
+        perror(message);
+        delete [] message;
+        exit(errno);   // crucial to exit so that the function does not
+                       // return twice!
+    }
+
+    else { // parent just waits for child completion
+        waitpid(childp, &status, 0);
+        // we restore the signal handler now that our baby answered
+        signal(SIGCHLD, zombie_reaper);
+        return status;
+    }
 }
 
-//monitor is there to collect data
-monitor_t mon;
 
-void* monitor (void* ignored){
-    // open file 'prev_command' upon CPRINT
-    //collect the  result from the client command (output)
-        // how to collect the RESULT of execve and copy it through a file?
-        // how to copy/paste the same result the status code message back to file??
-            //how to printf to client? it it using readline?
-    //close the file 'prev_command'
-}
-
-void* do_client (int sd){
+void* do_client (int sd, char** envp){
     const int ALEN = 256;
     char req[ALEN];
-    int line;
+    const char* ack = "ACK: ";
+    int line;           // to receive the command
+    
+    //to receive the client command
+    char command[129];   // buffer for commands
+    command[128] = '\0';
+    char* com_tok[129];  // buffer for the tokenized commands
+    size_t num_tok;      // number of tokens
 
     //the message line
     char status[5];
     int code;
-    char code_num[50];
     char mess[ALEN];
 
     printf("Incoming client... \n");
 
-    while ( (line = readline(sd, req, ALEN-1)) != 0){
+    signal(SIGCHLD, zombie_reaper);
+
+    while ( (line = readline(sd, req, ALEN-1)) != recv_nodata){
         memset(status, 0, 5);
-        memset(code_num, 0, sizeof(code_num));
         memset(mess, 0, sizeof(mess));
 
         if (strcmp(req, "quit") == 0){
             printf("quit command: sending EOF.\n");
-    
-            status = "OK ";
+
+            snprintf(status, 3, "OK");
             code = 0;
-            sprintf(code_num, "%d", code);
-            send(sd,status, strlen(status), 0);
-            send(sd,code_num, strlen(code_num), 0);
+            sprintf(mess, "%s %d close tcp connection\n", status, code);
+            
+            send(sd, ack, strlen(ack), 0);
+            send(sd, mess, strlen(mess), 0);
             shutdown(sd,1);
             close(sd);
             printf("Outgoing client...\n");
             return NULL;
         }
+        //just to be safe, even though readline already done so...
+        if (strlen(command) > 0 && command[strlen(command)-1] =='\n')
+            command[strlen(command) -1] = '\0';
+        
+        //tokenize input:
+        num_tok = str_tokenize(command, com_tok, strlen(command));
+        com_tok[num_tok] = 0;   //null termination fro execve
+        
+
+        // do we need to define exit???
+
+        int r = run_it(com_tok[0], com_tok, envp, path);
+        send(sd,ack, strlen(ack),0);
+        send(sd,req, strlen(req),0);
+        send(sd,"\n", 1,0);
     }
+
+    //read 0 bytes = EOF
+    shutdown(sd,1);
+    close(sd);
+    printf("Outgoing client...\n");
+    return NULL;
 }
 
 int main (int argc, char** argv, char** envp){
@@ -93,7 +167,7 @@ int main (int argc, char** argv, char** envp){
 
     long int msock, ssock;                              //master & slave socket
     struct sockaddr_in client_addr;                     // address of client
-    unsigned int client_addr_len = sizeof(cient_addr);  // and client addr length
+    unsigned int client_addr_len = sizeof(client_addr);  // and client addr length
 
     msock = passivesocket(port, qlen);
     if (msock < 0){
@@ -109,8 +183,6 @@ int main (int argc, char** argv, char** envp){
     pthread_attr_init(&ta);
     pthread_attr_setdetachstate(&ta, PTHREAD_CREATE_DETACHED);
 
-    //create monitor thread:
-    pthread_create(&tt, &ta, monitor, NULL);
 
     while (1){
         //Accept connection:
@@ -127,7 +199,7 @@ int main (int argc, char** argv, char** envp){
             perror("pthread_create");
             return 1;
         }
-
+        //we start accepting other clients (main thread continues with the loop)
     }
 
     return 0; //it will never reach
