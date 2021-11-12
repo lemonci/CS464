9e40618e9acf9cafed7bc12643eb615a0a7dc84f
Wing-Shu <57225457+Wing-Shu@users.noreply.github.com>
2021-11-12 10:07:52 -0500

    Update fserv.h

--- assignment 3/fserv.h
+++ assignment 3/fserv.h
@@ -1,7 +1,25 @@
+
+#include <sys/wait.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <signal.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <iostream>
 
 void* do_client_f (int);
+void add_trailing_spaces(char *, int, int);
+int initiate_descriptor(void);
+int write_descriptor(int, char*,FILE*,int);
+int check_descriptor(char *);
+int create_file(char*);
+int delete_descriptor(int);
+int clear_descriptor(void);
