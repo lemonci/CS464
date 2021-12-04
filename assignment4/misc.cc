f86aed37cca4d68e407784aeb5a1e154e05a6fa5
Wing-Shu <57225457+Wing-Shu@users.noreply.github.com>
2021-12-03 22:34:08 -0500

    fix part 2, add part 3 still need test on sighup

--- assignment4/misc.cc
+++ assignment4/misc.cc
@@ -7,6 +7,15 @@
  */
 #include "shfd.h"
 
+// DEFAULT PORT TO LISTEN TO
+int shport = 9001;              // ports to listen to
+int fport = 9002;
+int pport = 10000;
+
+long int shsock, fsock, psock;              // master sockets
+
+
+
 /*
 * the number of peers involved in replication
 */
@@ -23,6 +32,10 @@ int incr_threads;
 int curr_threads = 0;
 int act_threads = 0;
 
+bool tdie = false;
+int to_die;
+
+
 /*
  * Log file
  */
@@ -30,10 +43,17 @@ const char* logfile = "shfd.log";
 const char* pidfile = "shfd.pid";
 
 /*
+ * true iff the threads in file server is alive (and kicking).
+ */
+bool talive;
+bool reboot = true;
+
+/*
  * true iff the file server is alive (and kicking).
  */
 bool falive;
 
+pthread_mutex_t thread_mutex;
 pthread_mutex_t logger_mutex;
 
 extern char **environ;
@@ -95,60 +115,151 @@ int next_arg(const char* line, char delim) {
     return arg_index;
 }
 
-void* file_server (int msock) {
-    int ssock;                      // slave sockets
-    struct sockaddr_in client_addr; // the address of the client...
-    socklen_t client_addr_len = sizeof(client_addr); // ... and its length
-    // Setting up the thread creation:
+
+/*
+*Create preallocated thread in master socket
+* 0 = threads are create
+* 1= threads cannot be create
+* if reach the max_threads just go back....
+*/
+int set_threads(long int msock) {
+     // Setting up the thread creation:
     pthread_t tt;
     pthread_attr_t ta;
     pthread_attr_init(&ta);
     pthread_attr_setdetachstate(&ta,PTHREAD_CREATE_DETACHED);
+    
+    char msg[MAX_LEN];
 
-    char msg[MAX_LEN];  // logger string
-
-    while (1) {
-        // Accept connection:
-        ssock = accept(msock, (struct sockaddr*)&client_addr, &client_addr_len);
-       
-        //additonal t_incr threads
-
-        if (ssock < 0) {
-            if (errno == EINTR) continue;
-            snprintf(msg, MAX_LEN, "%s: file server accept: %s\n", __FILE__, strerror(errno));
+    for (int i=0; i< incr_threads; i++){
+        if (curr_threads <max_threads){
+            pthread_mutex_lock(&thread_mutex);
+            curr_threads++;
+            snprintf(msg, MAX_LEN, "%s: new thread [%d] create...current thread = %d\n", __FILE__, gettid(), curr_threads);
             logger(msg);
-            snprintf(msg, MAX_LEN, "%s: the file server died.\n", __FILE__);
-            logger(msg);
-            falive = false;
-            return 0;
+            pthread_mutex_unlock(&thread_mutex);
+            if(pthread_create(&tt, &ta, (void* (*) (void*))file_client, (void*) msock) != 0){
+                snprintf(msg, MAX_LEN, "%s: set threads cannot pthread_create: %s\n", __FILE__, strerror(errno));
+                logger(msg);
+                snprintf(msg, MAX_LEN, "%s: the file server died.\n", __FILE__);
+                logger(msg);
+                pthread_mutex_lock(&thread_mutex);
+                curr_threads--;
+                pthread_mutex_unlock(&thread_mutex);
+                talive = false;         //something is wrong with the client thread we should end procedure
+                return 1;
+            }            
         }
-
-        // assemble client coordinates (communication socket + IP)
-        client_t* clnt = new client_t;
-        clnt -> sd = ssock;
-        ip_to_dotted(client_addr.sin_addr.s_addr, clnt -> ip);
-
-        // create a new thread for the incoming client:
-        if ( pthread_create(&tt, &ta, (void* (*) (void*))file_client, (void*)clnt) != 0 ) {
-            snprintf(msg, MAX_LEN, "%s: file server pthread_create: %s\n", __FILE__, strerror(errno));
-            logger(msg);
-            snprintf(msg, MAX_LEN, "%s: the file server died.\n", __FILE__);
-            logger(msg);
-            falive = false;
-            return 0;
+        else{
+            break;     // reach max_threads limit
         }
-        // go back and block on accept.
     }
-    return 0;   // will never reach this anyway...
+    return 0;                 // meaning the threads have succeed
 }
 
-void setupfs() {
-     // Setting up the thread creation:
+/**
+ * handle threads whether to die or not
+ * 
+ */
+void handle_threads(){
+        char msg[MAX_LEN];
+
+        pthread_mutex_lock(&thread_mutex);
+        if (tdie){
+            to_die --;
+            curr_threads --;
+            if (to_die == 0)
+                tdie = false;            
+        }
+        else{
+            // tdie is back to false
+            pthread_mutex_unlock(&thread_mutex);
+            return;
+        }
+        pthread_mutex_unlock(&thread_mutex);
+        snprintf(msg, MAX_LEN, "%s: idle thread [%d] has died\n", __FILE__, gettid());
+        logger(msg);
+        pthread_exit(NULL);
+    
+}
+
+void* file_server (int msock) {
+    char msg[MAX_LEN];
+
+    talive = true;              //we have our initial threads
+    if (set_threads(msock) != 0){              //set the initial threads
+        snprintf(msg, MAX_LEN, "%s: file server failed to make new threads from set_threads\n", __FILE__);
+        logger(msg);
+        snprintf(msg, MAX_LEN, "%s: the file server died.\n", __FILE__);
+        logger(msg);
+        talive = false;
+        falive = false;             //we don't have client threads so file server is useless...
+        return 0;
+    }
+
+    while (talive){              //keep the initial threads alive 
+        sleep(3);              //verify every 1 min if thread are alive?
+    }
+    
+    //now that talive is false, all threads should self destruct
+    // delete all the remaining threads - active or idle to prevent further problem 
+    while (curr_threads != 0){
+        handle_threads();
+    }
+    falive = false;             //we should also terminate the file server thread too
+
+    snprintf(msg, MAX_LEN, "%s: file server ending\n", __FILE__);
+    logger(msg);
+
+    return 0;   //it will reach if something bad happen....
+
+
+/****************************old code*************************************/
+    // int ssock;                      // slave sockets
+    // struct sockaddr_in client_addr; // the address of the client...
+    // socklen_t client_addr_len = sizeof(client_addr); // ... and its length
+    // // Setting up the thread creation:
     // pthread_t tt;
     // pthread_attr_t ta;
     // pthread_attr_init(&ta);
     // pthread_attr_setdetachstate(&ta,PTHREAD_CREATE_DETACHED);
+
+    // char msg[MAX_LEN];  // logger string
+
+    // while (1) {
+    //     // Accept connection:
+    //     ssock = accept(msock, (struct sockaddr*)&client_addr, &client_addr_len);
+       
+    //     //additonal t_incr threads
+
+    //     if (ssock < 0) {
+    //         if (errno == EINTR) continue;
+    //         snprintf(msg, MAX_LEN, "%s: file server accept: %s\n", __FILE__, strerror(errno));
+    //         logger(msg);
+    //         snprintf(msg, MAX_LEN, "%s: the file server died.\n", __FILE__);
+    //         logger(msg);
+    //         falive = false;
+    //         return 0;
+    //     }
+
+    //     // assemble client coordinates (communication socket + IP)
+    //     client_t* clnt = new client_t;
+    //     clnt -> sd = ssock;
+    //     ip_to_dotted(client_addr.sin_addr.s_addr, clnt -> ip);
+
+    //     // create a new thread for the incoming client:
+    //     if ( pthread_create(&tt, &ta, (void* (*) (void*))file_client, (void*)clnt) != 0 ) {
+    //         snprintf(msg, MAX_LEN, "%s: file server pthread_create: %s\n", __FILE__, strerror(errno));
+    //         logger(msg);
+    //         snprintf(msg, MAX_LEN, "%s: the file server died.\n", __FILE__);
+    //         logger(msg);
+    //         falive = false;
+    //         return 0;
+    //     }
+    //     // go back and block on accept.
+    // }
 }
+
 
 void* shell_server (int msock) {
     int ssock;                      // slave sockets
@@ -163,7 +274,27 @@ void* shell_server (int msock) {
     char msg[MAX_LEN];  // logger string
 
     while (1) {
-        // Accept connection:
+        //check the status of the falive, else we exit 
+        struct pollfd pollrec;
+        pollrec.fd = msock;
+        pollrec.events = POLLIN;
+
+        int polled = poll (&pollrec, 1, TIME_EVAL);
+
+        if(polled == -1){ //err in poll
+            snprintf(msg, MAX_LEN, "%s: shell server poll: %s\n", __FILE__, strerror(errno));
+            logger(msg);
+            return 0;
+        }
+        if(polled == 0){    //no client...
+            // if file server dies... so does the shell server
+            if (!falive){
+                break;
+            }
+            continue;   //continue to poll until there is a client
+        }        
+
+        // Accept connection if poll == 1:
         ssock = accept(msock, (struct sockaddr*)&client_addr, &client_addr_len);
         if (ssock < 0) {
             if (errno == EINTR) continue;
@@ -187,7 +318,11 @@ void* shell_server (int msock) {
         //only one connection at a time from incoming client:
         shell_client(clnt);
 
-        // go back and block on accept.
+        // go back and block on poll.
+    }
+    if (!falive){
+        snprintf(msg, MAX_LEN, "%s: shell server died due to falive = 0\n", __FILE__);
+        logger(msg);
     }
     return 0;   // will never reach this anyway...
 }
@@ -224,16 +359,128 @@ void extractPeer(char* address){
     }
 }
 
+void deal_SIGHUP( int num){
+    char msg[MAX_LEN]; 
+    const int qlen = 32;            // queue length for incoming connections
+
+    snprintf(msg,MAX_LEN,"%s:RECEIVE SIGHUP\n", __FILE__);
+    logger(msg);
+    talive = false;         //end all the threads
+    falive = false;         //end the file server
+    snprintf(msg,MAX_LEN,"%s:THREADS ARE NOW DELETING AND CLOSING...wait 15 sec\n", __FILE__);
+    logger(msg);
+    
+    sleep(15);
+    snprintf(msg,MAX_LEN,"%s:THREADS ARE NOW DELETED\n", __FILE__);
+    logger(msg);
+
+    shutdown(shsock, SHUT_RDWR);
+    close(shsock);
+    snprintf(msg, MAX_LEN, "%s: shell server died, exiting.\n", __FILE__);
+    logger(msg);
+    shutdown(fsock, SHUT_RDWR);
+    close(fsock);
+    snprintf(msg, MAX_LEN, "%s: file server died, exiting.\n", __FILE__);
+    logger(msg);
+    shutdown(psock, SHUT_RDWR);
+    close(psock);
+    snprintf(msg, MAX_LEN, "%s:peer server died, exiting.\n", __FILE__);
+    logger(msg);
+
+    //rebooting
+    snprintf(msg, MAX_LEN, "%s: REBOOTING THE SYSTEM...\n", __FILE__);
+    logger(msg);
+
+    //reassign the global variables back to its initial state
+    pthread_mutex_lock(&thread_mutex);
+    to_die = incr_threads;
+    curr_threads = 0; 
+    act_threads = 0;
+    pthread_mutex_unlock(&thread_mutex);
+
+    //reassign the threads
+    shsock = controlsocket(shport,qlen);
+    if (shsock < 0) {
+        perror("shell server controlsocket");                                              // now only listen fro local connections i.e. 127.0.0.1
+        return;
+    }
+    printf("Shell server up and listening clients on local machine at port %d\n", shport);
+
+    fsock = passivesocket(fport,qlen);
+    if (fsock < 0) {
+        perror("file server passivesocket");
+        return;
+    }
+    printf("File server up and listening on port %d\n", fport);
+
+    psock = passivesocket(pport,qlen);
+    if (psock < 0) {
+        perror("peer server passivesocket");
+        return;
+    }
+    printf("peer server up and listening on port %d\n", pport);
+
+    // Setting up the thread creation:
+    pthread_t tt;
+    pthread_attr_t ta;
+    pthread_attr_init(&ta);
+    pthread_attr_setdetachstate(&ta,PTHREAD_CREATE_DETACHED);
+
+    // Launch the thread that becomes a file server:
+    if ( pthread_create(&tt, &ta, (void* (*) (void*))file_server, (void*)fsock) != 0 ) {
+        snprintf(msg, MAX_LEN, "%s: pthread_create: %s\n", __FILE__, strerror(errno));
+        logger(msg);
+        return;
+    }
+    falive = true;
+
+    // Continue and become the shell server:
+    shell_server(shsock);
+    
+}
+
+void deal_SIGQUIT(int num){
+    char msg[MAX_LEN];
+    reboot = false;         // we want to shutdown the servers
+    
+    snprintf(msg,MAX_LEN,"%s:RECEIVE SIGQUIT\n", __FILE__);
+    logger(msg);
+    talive = false;         //end all the threads
+    falive = false;         //end the file server
+    snprintf(msg,MAX_LEN,"%s:THREADS ARE NOW DELETING AND CLOSING...wait 15 sec\n", __FILE__);
+    logger(msg);
+    
+    sleep(15);
+    snprintf(msg,MAX_LEN,"%s:THREADS ARE NOW DELETED\n", __FILE__);
+    logger(msg);
+    
+
+    shutdown(shsock, SHUT_RDWR);
+    close(shsock);
+    snprintf(msg, MAX_LEN, "%s: shsock is closed, exiting.\n", __FILE__);
+    logger(msg);
+    shutdown(fsock, SHUT_RDWR);
+    close(fsock);
+    snprintf(msg, MAX_LEN, "%s: fsock is closed, exiting.\n", __FILE__);
+    logger(msg);
+    shutdown(psock, SHUT_RDWR);
+    close(psock);
+    snprintf(msg, MAX_LEN, "%s: psock is closed, exiting.\n", __FILE__);
+    logger(msg);
+
+    // snprintf(msg, MAX_LEN, "%s: all the servers died, exiting.\n", __FILE__);
+    // logger(msg);
+
+    exit(num);
+}
+
 /*
  * Initializes the access control structures, fires up a thread that
  * handles the file server, and then does the standard job of the main
  * function in a multithreaded shell server.
  */
 int main (int argc, char** argv, char** envp) {
-    int shport = 9001;              // ports to listen to
-    int fport = 9002;
-    int pport = 10000;
-    long int shsock, fsock;              // master sockets
+
     const int qlen = 32;            // queue length for incoming connections
     char* progname = basename(argv[0]);  // informational use only.
     max_threads = 12;
@@ -244,6 +491,7 @@ int main (int argc, char** argv, char** envp) {
     char msg[MAX_LEN];  // logger string
 
     pthread_mutex_init(&logger_mutex, 0);
+    pthread_mutex_init(&thread_mutex, 0);
 
     // parse command line
     extern char *optarg;
@@ -284,7 +532,8 @@ int main (int argc, char** argv, char** envp) {
         }
     }
     //the extra arguments we extract and put them in the struct Peer
-    for(; optind < argc; optind++){
+    optind --;
+    for(; optind < argc && *argv[optind] != '-'; optind++){
         extractPeer(argv[optind]);
     }          
 
@@ -297,17 +546,20 @@ int main (int argc, char** argv, char** envp) {
         printf("Cannot create threads because t_incr > t_max\n");
         return 1;
     }
+    printf("File server creates %d threads with a maximum threads of %d\n", incr_threads, max_threads);
 
     if (replica == 0){
         printf("no peer for replication. Synchronization off at port %d\n", pport);
     }
     else{
         printf("peers detected...Synchronization on at port %d\n", pport);
-        for (int i=0; i< replica; i++){
+        for (int i=0; i< replica; i++){ 
             printf("peer %d: host-> %s  port-> %d\n", i, pserv[i].phost, pserv[i].pport);
             // some flag for synchronization
         }
     }
+
+    
     // The pid file does not make sense as a lock file since our
     // server never goes down willlingly.  So we do not lock the file,
     // we just store the pid therein.  In other words, we hint to the
@@ -330,6 +582,7 @@ int main (int argc, char** argv, char** envp) {
     // Open the master sockets (this is the startup code, since we
     // might not have permissions to open this socket for some reason
     // or another, case in which the startup fails):
+    
     shsock = controlsocket(shport,qlen);
     if (shsock < 0) {
         perror("shell server controlsocket");                                              // now only listen fro local connections i.e. 127.0.0.1
@@ -344,6 +597,17 @@ int main (int argc, char** argv, char** envp) {
     }
     printf("File server up and listening on port %d\n", fport);
 
+    psock = passivesocket(pport,qlen);
+    if (psock < 0) {
+        perror("peer server passivesocket");
+        return 1;
+    }
+    printf("peer server up and listening on port %d\n", pport);
+
+    //PART 3 - HANDLE SIGNALS
+    signal(SIGHUP, deal_SIGHUP);
+    signal(SIGQUIT, deal_SIGQUIT);
+
     // ... and we detach!
     if (detach) {
         // umask:
@@ -351,9 +615,9 @@ int main (int argc, char** argv, char** envp) {
 
         // ignore SIGHUP, SIGINT, SIGQUIT, SIGTERM, SIGALRM, SIGSTOP:
         // (we do not need to do anything about SIGTSTP, SIGTTIN, SIGTTOU)
-        signal(SIGHUP,  SIG_IGN);
+        //signal(SIGHUP,  SIG_IGN);
         signal(SIGINT,  SIG_IGN);
-        signal(SIGQUIT, SIG_IGN);
+        //signal(SIGQUIT, SIG_IGN);
         signal(SIGTERM, SIG_IGN);
         signal(SIGALRM, SIG_IGN);
         signal(SIGSTOP, SIG_IGN);
@@ -405,15 +669,20 @@ int main (int argc, char** argv, char** envp) {
     shell_server(shsock);
 
     // If we get this far the shell server has died 
-    snprintf(msg, MAX_LEN, "%s: the shell server died.\n", __FILE__);
+    snprintf(msg, MAX_LEN, "%s: the shell server died from main.\n", __FILE__);
     logger(msg);
     // keep this thread alive for the file server
     while (falive) {
-        sleep(30);
+        sleep(5);
     }
-
-    snprintf(msg, MAX_LEN, "%s: all the servers died, exiting.\n", __FILE__);
-    logger(msg);
+    // keep the process alive  for a restart
+    while(reboot){
+        sleep(5);
+    }
     
+    //no reboot, poor file and shell server have died....
+    snprintf(msg, MAX_LEN, "%s: all the servers died, exiting from main.\n", __FILE__);
+    logger(msg);
+
     return 1;
 }
