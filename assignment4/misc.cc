acd5ac18b6ff067726a5d823aac0bb46ebc8e4cd
lemonci <hongyanchangfei@gmail.com>
2021-12-18 10:50:40 -0500

    Update misc.cc

--- assignment4/misc.cc
+++ assignment4/misc.cc
@@ -190,11 +190,11 @@ void handle_threads(){
     
 }
 
-void* file_server (int msock) {
+void* file_server (int msock, int client) {
     char msg[MAX_LEN];
 
     talive = true;              //we have our initial threads
-    if (set_threads(msock) != 0){              //set the initial threads
+    if (set_threads(msock, client) != 0){              //set the initial threads
         snprintf(msg, MAX_LEN, "%s: file server failed to make new threads from set_threads\n", __FILE__);
         logger(msg);
         snprintf(msg, MAX_LEN, "%s: the file server died.\n", __FILE__);
