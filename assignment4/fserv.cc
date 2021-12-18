dea145256dbbb02f5e90f45c1be5e552cf1b8027
lemonci <hongyanchangfei@gmail.com>
2021-12-17 19:47:17 -0500

    Update fserv.cc

--- assignment4/fserv.cc
+++ assignment4/fserv.cc
@@ -583,10 +583,11 @@ void* file_client (int msock) {
                 if (idx1 == -1) // no identifier
                     snprintf(ans,MAX_LEN,"FAIL %d FREAD requires a number of bytes to read", EBADMSG);
                 else {
-					printf("idx: %d, idx1: %d, req:%s\n", idx, idx1, req);
+					snprintf(msg, MAX_LEN,"idx: %d, idx1: %d, req:%s\n", idx, idx1, req);
+					logger(msg);
                     idx1 = idx + idx1;
                     req[idx1 - 1] = '\0';
-					printf("req_afterwards:%s\n", req);
+					snprintf(msg, MAX_LEN, "req_afterwards:%s\n", req);
                     if (debugs[DEBUG_COMM]) {
                         snprintf(msg, MAX_LEN, "%s: (before decoding) will read %s bytes from %s \n",
                                  __FILE__, &req[idx1], &req[idx]); 
